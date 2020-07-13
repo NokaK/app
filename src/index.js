563090504b9cabd8899a11a1651df52e617ab33e
davidkvara <davekvaratskhelia@gmail.com>
2020-07-14 00:20:43 +0400

    remove snap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import { hydrate, render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+ReactDOM.render(<App />, document.getElementById('root'));
